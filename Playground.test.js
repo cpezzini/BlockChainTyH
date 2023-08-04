42a19f52c9df13e89f59675cdc38e25f915b4077
mpaladino <marcelpaladino@yahoo.com.ar>
2023-08-03 23:19:50 -0300

    Código ordenado

--- Playground.test.js
+++ Playground.test.js
@@ -17,13 +17,9 @@ describe('Pruebas Blockchain', () => {
   let hashStrategy;
   beforeEach(() => {
     // Inicializar el hashStrategy según el tipoHash
-    
     hashStrategy = tipoHash === "SHA256" ? new SHA256HashStrategy() : new MD5HashStrategy();
   });
-
-  
   describe('Nodo', () => {
-
     test('Nodo - Cierre de bloque', () => {
       const tipoHash = "MD5";
       hashStrategy = tipoHash === "MD5" ?  new MD5HashStrategy() : new SHA256HashStrategy();
@@ -61,7 +57,6 @@ describe('Pruebas Blockchain', () => {
       nodo1.agregarTransaccion(transaccion9);
       blockchain.push(nodo1.devolverBloqueCerrado());
      });
-    
     test('Nodo - Agregar más de 10 transacciones (forzar error)', () => {
       const tipoHash = "MD5";
       hashStrategy = tipoHash === "MD5" ?  new MD5HashStrategy() : new SHA256HashStrategy();
@@ -98,7 +93,6 @@ describe('Pruebas Blockchain', () => {
       const transaccion9 = new TransaccionSimple('id10', transaccion8, 'direccion10', hashStrategy);
       nodo1.agregarTransaccion(transaccion9);
       const transaccion10 = new TransaccionSimple('id11', transaccion9, 'direccion11', hashStrategy);
-      
       expect(() => nodo1.agregarTransaccion(transaccion10)).toThrow(Error);
     });
     test('Nodo - Agregar Transaccion Compuesta al bloque abierto', () => {
@@ -110,7 +104,7 @@ describe('Pruebas Blockchain', () => {
         nodo.agregarNodo(nodo1);
       });
       nodos.push(nodo1);
-     var coinbase3 = new Coinbase('id3', 'direccion3', hashStrategy);
+      var coinbase3 = new Coinbase('id3', 'direccion3', hashStrategy);
       nodo1.agregarCoinbase(coinbase3);
       const trs1 = new TransaccionSimple('id4', coinbase3, 'direccion4', hashStrategy);
       nodo1.agregarTransaccion(trs1);
@@ -123,10 +117,8 @@ describe('Pruebas Blockchain', () => {
       trc1.agregarTransaccionHija(trs4,'2');
       console.log (trc1.cantidadTransacciones());
       nodo1.agregarTransaccion(trc1);
-   });
-
+    });
   });
-
   describe('Bloque', () => {
     test('Bloque - Agregar Coinbase', () => {
       let blockchain = [];
@@ -151,7 +143,6 @@ describe('Pruebas Blockchain', () => {
       const coinbase = new Coinbase('id1', 'direccion1', hashStrategy);
       nodo1.agregarCoinbase(coinbase);
       const coinbase2 = new Coinbase('id2', 'direccion2', hashStrategy);
-      
       expect(() => nodo1.agregarCoinbase(coinbase2)).toThrow(Error);
     });
     test('Bloque - Agregar Coinbase con hash erroneo (forzar error)', () => {
@@ -163,13 +154,10 @@ describe('Pruebas Blockchain', () => {
         nodo.agregarNodo(nodo1);
       });
       nodos.push(nodo1);
-      
       // Crea una instancia de Coinbase con datos válidos
       const coinbase = new Coinbase('id1', 'direccion1', xhashStrategy);
-      
       // Modifica el hash de la instancia de Coinbase para que sea incorrecto
       coinbase._hash = 'hash_erroneo';
-      
       expect(() => nodo1.agregarCoinbase(coinbase)).toThrow(Error);
     });
     test('Bloque - Agregar transacciones y al agregar la 9 esta es erronea(forzar error)', () => {
@@ -186,7 +174,6 @@ describe('Pruebas Blockchain', () => {
       nodos.forEach((nodo) => { 
         nodo.agregarNodo(nodo2);
       });
-      
       nodos.push(nodo2);
       const coinbase = new Coinbase('id1', 'direccion1', hashStrategy);
       nodo1.agregarCoinbase(coinbase);
@@ -211,7 +198,6 @@ describe('Pruebas Blockchain', () => {
       transaccion9._hash = 'hash_erroneo';
       expect(() => nodo1.agregarTransaccion(transaccion9)).toThrow(Error);
     });
-  
   });
   describe('Token', () => {
     test('Token - Obtener Token', () => {
@@ -245,7 +231,6 @@ describe('Pruebas Blockchain', () => {
       const xtrs5 = new TransaccionSimple('id8', xtrs4, 'direccion8', xhashStrategy);
       expect(() => xtrc1.agregarTransaccionHija(xtrs5, '2')).toThrow(Error);
     });
-
     test('Transaccion Compuesta completa - 2 niveles de compuesta ', () => {
       var xhashStrategy = tipoHash === "SHA256" ? new SHA256HashStrategy() : new MD5HashStrategy();
       let blockchain = [];
@@ -302,8 +287,6 @@ describe('Pruebas Blockchain', () => {
       xtrc3.agregarTransaccionHija(xtrs7,'3'); 
       expect(() => xtrc2.agregarTransaccionHija(xtrc3,'3')).toThrow(Error);
     });
-
-
     test('Transaccion Compuesta- Intento agregar una Transaccion Inválida (hash inválido) a una Transaccion Compuesta (forzar error)', () => {
       var xhashStrategy = tipoHash === "SHA256" ? new SHA256HashStrategy() : new MD5HashStrategy();
       let blockchain = [];
@@ -313,46 +296,39 @@ describe('Pruebas Blockchain', () => {
           nodo.agregarNodo(xnodo1);
       });
       xnodos.push(xnodo1);
-    
       var xcoinbase3 = new Coinbase('id3', 'direccion3', xhashStrategy);
       xnodo1.agregarCoinbase(xcoinbase3);
-    
       const xtrs1 = new TransaccionSimple('id4', xcoinbase3, 'direccion4', xhashStrategy);
       xnodo1.agregarTransaccion(xtrs1);
-    
       var xtrc1 = new TransaccionCompuesta(hashStrategy);
-    
       const xtrs2 = new TransaccionSimple('id5', xtrs1, 'direccion5', xhashStrategy);
       // modificamos el hash de xtrs2 para que falle la validación
       xtrs2._hash = 'hash_invalido';
-      
       expect(() => xtrc1.agregarTransaccionHija(xtrs2,'2')).toThrow(Error);
       const xtrs3 = new TransaccionSimple('id6', xtrs2, 'direccion6', xhashStrategy);
       xtrc1.agregarTransaccionHija(xtrs3,'2');
-    
       const xtrs4 = new TransaccionSimple('id7', xtrs3, 'direccion7', xhashStrategy);
       xtrc1.agregarTransaccionHija(xtrs4,'2');
-    
       const xtrs5 = new TransaccionSimple('id8', xtrs4, 'direccion8', xhashStrategy);
       xtrc1.agregarTransaccionHija(xtrs5,'2');
     });
-});
-describe('HashStrategy', () => {
-  test('HashStrategy - Forzar error al usar clase abstracta', () => {
-    const hashStrategy = new HashStrategy();
-    expect(() => hashStrategy.calculateHash('data')).toThrowError('calculateHash debe ser implementado por las clases hijas');
   });
-  test('MD5HashStrategy - Calcular Hash', () => {
-    const data = 'data';
-    const md5HashStrategy = new MD5HashStrategy();
-    const hash = md5HashStrategy.calculateHash(data);
-    expect(hash).toBeDefined();
+  describe('HashStrategy', () => {
+    test('HashStrategy - Forzar error al usar clase abstracta', () => {
+      const hashStrategy = new HashStrategy();
+      expect(() => hashStrategy.calculateHash('data')).toThrowError('calculateHash debe ser implementado por las clases hijas');
+    });
+    test('MD5HashStrategy - Calcular Hash', () => {
+      const data = 'data';
+      const md5HashStrategy = new MD5HashStrategy();
+      const hash = md5HashStrategy.calculateHash(data);
+      expect(hash).toBeDefined();
+    });
+    test('SHA256HashStrategy - Calcular Hash', () => {
+      const data = 'data';
+      const sha256HashStrategy = new SHA256HashStrategy();
+      const hash = sha256HashStrategy.calculateHash(data);
+      expect(hash).toBeDefined();
+    });
   });
-  test('SHA256HashStrategy - Calcular Hash', () => {
-    const data = 'data';
-    const sha256HashStrategy = new SHA256HashStrategy();
-    const hash = sha256HashStrategy.calculateHash(data);
-    expect(hash).toBeDefined();
-  });
-});
 });
